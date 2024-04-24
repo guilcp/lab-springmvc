0b3022f4b35d1b9485aa902ec371966ff81500ce
Guilherme Luís Carvalho <blcpinto@sga.pucminas.br>
2024-04-24 20:08:31 -0300

    a

---
+++ src/main/java/com/labspringmvc/labspringmvc/LabSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package com.labspringmvc.labspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labspringmvc/labspringmvc/LabSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labspringmvc.labspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
